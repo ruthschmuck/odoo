f99d2f7c383235b881cf6fa5d6e47ca872905515
Antoni Ruth <antoniruthschmuck@gmail.com>
2017-05-25 23:23:11 -0400

    arreglando el bad request

--- new_modules/models/print2.py
+++ new_modules/models/print2.py
@@ -7,4 +7,4 @@ class print2(models.Model):
     @api.multi
     def my_button(self):
         self.write({'state': "sent"})
-        return self.env['report'].get_action(self, 'my_module.report_purchasequotation')
+        return self.env['report'].get_action(self, 'my_module.antoni_report_saleorder')
