aae4e4aee54ccda0fad73ffb3458fddd2a81308d
Antoni Ruth <antoniruthschmuck@gmail.com>
2017-05-25 22:29:06 -0400

    arreglando carpeta models, nuevos errores

--- new_modules/models/print2.py
+++ new_modules/models/print2.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 from odoo import models, fields, api
 
-class print2(models.Moldel):
+class print2(models.Model):
     _inherit = "sale.order"
 
     @api.multi
     def my_button(self):
         self.write({'state': "sent"})
-        return self.env['report'].get_action(self, 'purchase.report_purchasequotation')
+        return self.env['report'].get_action(self, 'my_module.report_purchasequotation')
